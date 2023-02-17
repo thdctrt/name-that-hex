ac7d0077e16d4941d9331fca68e4f622c8816fd7
Timur Tokunov <0@447.kz>
2023-02-18 00:10:21 +0100

    initial commit

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import styled from "styled-components";
+
+const HEXInput: React.FC = () => {
+  const [value, setValue] = React.useState<string>(`${typeof window !== "undefined" && localStorage.getItem("hex-hex") ? localStorage.getItem("hex-hex") : ""}`);
+  // const [value, setValue] = React.useState<string>("");
+  const [prompt, setPrompt] = React.useState<string>("");
+  const [color, setColor] = React.useState<string>("");
+  // const [colorBlock, setColorBlock] = React.useState();
+  const [isActive, setActive] = useState("false");
+
+  const ColorBlock = styled.div`
+    background: ${typeof window !== "undefined"
+      ? localStorage.getItem("hex-hex")
+      : "transparent"};
+  `;
+
+  const Button = styled.button`
+    .visible {
+      visibility: visible;
+    }
+    .hidden {
+      visibility: hidden;
+    }
+  `
+
+  const handleInput = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+      console.log(e.target.value);
+    },
+    []
+  );
+
+  const handleKeyDown = React.useCallback(
+    async (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        setPrompt(value);
+        localStorage.setItem("hex-hex", value);
+
+        setColor("Loading...");
+        const response = await fetch("/api/openai", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: value }),
+        });
+        const data = await response.json();
+        setColor(`${data.result.choices[0].text}`);
+      }
+    },
+    [value]
+  );
+
+  const handleClick = useCallback(async () => {
+    if (localStorage.getItem("hex-hex")) {
+      //scenario 1 - value in local storage
+      let value = localStorage.getItem("hex-hex");
+      setPrompt(value);
+      console.log("got " + value + " from local storage");
+      setColor("Loading...");
+      const response = await fetch("/api/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: value }),
+      });
+      const data = await response.json();
+      setColor(`${data.result.choices[0].text}`);
+      // setColorBlock(value);
+      //end scenario 1
+
+      //scenario 2 - no local storage but value in input
+    } else if (value && localStorage.getItem("hex-hex")) {
+      localStorage.setItem("hex-hex", value);
+      console.log('dealed with that shit');
+    //scenario 2.5
+    } else if (value) {
+      localStorage.setItem("hex-hex", value);
+      setPrompt(value);
+      console.log(value);
+      setColor("Loading...");
+      const response = await fetch("/api/openai", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: value }),
+      });
+      const data = await response.json();
+      setColor(`${data.result.choices[0].text}`);
+      //end scenario 2
+      //scenario 3 — input not filled
+    } else {
+      alert("no value");
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <div className="mt-1 relative flex items-center">
+          <input
+            aria-label="Enter your HEX"
+            onChange={handleInput}
+            onKeyDown={handleKeyDown}
+            onBlur={setActive(!isActive)}
+            type="text"
+            name="description"
+            // placeholder="Enter your color HEX"
+            defaultValue={value}
+            // value={value ? value : null}
+          />
+        </div>
+        {color ? 
+        <Button onClick={handleClick} className={isActive ? "visible" : "hidden"}>
+          {!color ? "Find" : "Find again"}
+        </Button>
+          : null  
+          }
+        <div className="mt-6 h-16">
+          {color && (
+            <ColorBlock>
+              <h2>Color Name:</h2>
+              {color}
+            </ColorBlock>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HEXInput;
---
+++ src/pages/api/openai.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `name for the color #${req.body.text} in camel case`,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+//   console.log(req.body.text);
+  res.status(200).json({ result: response.data });
+}
